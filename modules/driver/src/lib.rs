b0d0d69448b6f02abf3031a499c961885a43d726
Nishant Joshi <nishant.joshi@juspay.in>
2024-12-26 18:39:47 +0530

    chore: add driver implementation

--- modules/driver/src/lib.rs
+++ modules/driver/src/lib.rs
@@ -1,3 +1,37 @@
+#[repr(C)]
+pub struct CString {
+    ptr: *const u8,
+    len: usize,
+}
+
 extern "C" {
-    fn 
+    fn get(key_ptr: *const u8, key_len: usize) -> CString;
+    fn set(key_ptr: *const u8, key_len: usize, value_ptr: *const u8, value_len: usize);
+}
+
+#[no_mangle]
+pub extern "C" fn intend(ptr: *const u8, len: usize) -> CString {
+    todo!()
+}
+
+#[no_mangle]
+pub extern "C" fn done(ptr: *const u8, len: usize) {
+    todo!()
+}
+
+#[no_mangle]
+pub extern "C" fn view(ptr: *const u8, len: usize) -> CString {
+    todo!()
+}
+
+#[no_mangle]
+pub extern "C" fn transfer(
+    data1_ptr: *const u8,
+    len1_ptr: usize,
+    data2_ptr: *const u8,
+    len2_ptr: usize,
+    data3_ptr: *const u8,
+    len3_ptr: usize,
+) {
+    todo!()
 }
