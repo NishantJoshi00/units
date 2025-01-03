bc33d65cbda990f0771f0588c91710b1080f8e65
Nishant Joshi <nishant.joshi@juspay.in>
2025-01-03 12:34:09 +0530

    chore: do some changes to the sample program

--- modules/user-module/src/safe_drivers.rs
+++ modules/user-module/src/safe_drivers.rs
@@ -30,52 +30,50 @@ fn safe_view(path: &str) -> String {
     unsafe { String::from_wasmptr(ptr, len) }
 }
 
-// pub fn safe_main(input: &str) -> &str {
-//     let input: Input = serde_json::from_str(input).expect("invalid input");
-//     let p1 = safe_intend(&input.path1);
-//     let p2 = safe_intend(&input.path2);
-
-//     let data = Data {
-//         amount: input.amount,
-//     };
-//     let data = serde_json::to_string(&data).expect("invalid data");
-
-//     safe_transfer(&p1, &p2, &data);
-
-//     safe_done(&p1);
-//     safe_done(&p2);
-
-//     "done"
-// }
-
 pub fn safe_main(input: &str) -> &str {
     let input: Input = serde_json::from_str(input).expect("invalid input");
     let p1 = safe_intend(&input.path1);
     let p2 = safe_intend(&input.path2);
 
-    let p1_data = safe_view(&p1);
-    let p2_data = safe_view(&p2);
-
-    let p1_d = serde_json::from_str::<Data>(&p1_data).expect("invalid data");
-    let p2_d = serde_json::from_str::<Data>(&p2_data).expect("invalid data");
-
-    let data = ViewData {
-        path1: p1_d,
-        path2: p2_d,
+    let data = Data {
+        amount: input.amount,
     };
+    let data = serde_json::to_string(&data).expect("invalid data");
+
+    safe_transfer(&p1, &p2, &data);
+
+    safe_done(&p1);
+    safe_done(&p2);
+
+
+    "done"
+}
+
+pub fn safe_main(input: &str) -> &str {
+    let input: OnlyPath = serde_json::from_str(input).expect("invalid input");
+    let p1 = safe_intend(&input.path);
+
+    let p1_data = safe_view(&p1);
+
+    let p1_d = serde_json::from_str::<MoreData>(&p1_data).expect("invalid data");
+
+    let data = ViewData { path1: p1_d };
 
     let data = serde_json::to_string(&data).expect("invalid data");
 
     safe_done(&p1);
-    safe_done(&p2);
 
     data.leak()
 }
 
 #[derive(serde::Serialize)]
 struct ViewData {
-    path1: Data,
-    path2: Data,
+    path1: MoreData,
+}
+
+#[derive(serde::Deserialize)]
+struct OnlyPath {
+    path: String,
 }
 
 #[derive(serde::Deserialize)]
@@ -90,4 +88,9 @@ struct Data {
     amount: u64,
 }
 
-// USER CODE
+
+#[derive(serde::Serialize, serde::Deserialize)]
+struct MoreData {
+    name: String,
+    amount: u64,
+}
